5602b3cc6165341d895ab44d9e766a66dfa1a58a
MD Javed <md.javed@newsignature.com>
2020-04-08 10:38:36 -0400

    Pdf Engine Upload/Download

---
+++ WebApi.React.AI/Controllers/PdfEngineController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.React.AI.Controllers
+{
+    public class PdfEngineController : ApiController
+    {
+        //upload to blob
+        public static void UploadFiles()
+        {
+            string storageConnectionString = "*****Value copied from the Console****";
+
+            DirectoryInfo directoryInfo = new DirectoryInfo("D:\\InputPath");
+
+            var files = directoryInfo.EnumerateFiles();
+
+            // Retrieve storage account from connection string.
+            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+            //// Create the blob client.
+            //CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            //// Retrieve reference to a previously created container.
+            //CloudBlobContainer container = blobClient.GetContainerReference("<<Blob Container Name>>");
+
+            //foreach (FileInfo inputFile in files)
+            //{
+
+            //    CloudBlockBlob blockBlob = container.GetBlockBlobReference("<<Folder(If required)>>\\" + inputFile.Name);
+
+            //    blockBlob.UploadFromFile(inputFile.FullName);
+            //}
+        }
+
+        //download from blob
+    }
+}
